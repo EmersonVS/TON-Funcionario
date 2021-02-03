cdc65a50d504b5137fe4c480d0ab2e0cdfb6acdd
EmersonVS <eme_valentim@hotmail.com>
2021-02-02 22:04:21 -0300

    feat: objeto retorno padrão para métodos

--- src/main/java/com/stone/challenge_1/app/controller/funcionario/DTO/FuncionarioDTO.java
+++ src/main/java/com/stone/challenge_1/app/controller/funcionario/DTO/FuncionarioDTO.java
@@ -1,5 +1,36 @@
 package com.stone.challenge_1.app.controller.funcionario.DTO;
 
+import com.stone.challenge_1.app.models.entity.Funcionario;
+
 public class FuncionarioDTO {
+
+	private Long id;
+	private String nome;
+	private String cargo;
+	private Integer idade;
+	
+	
+	public FuncionarioDTO(Funcionario newFuncionario) {
+		this.id = newFuncionario.getFuncionarioId();
+		this.nome = newFuncionario.getNome();
+		this.cargo = newFuncionario.getCargo();
+		this.idade = newFuncionario.getIdade();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
 
 }
